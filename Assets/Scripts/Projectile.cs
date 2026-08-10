9b6f51d407bb1ec6deb3294775a4db6d40cab083
GabrielFreitas836 <gabriel.freitas@sou.unifeob.edu.br>
2026-08-10 14:38:41 -0300

    change: alterar velocidade do projétil e tentativa de corrigir bug
    - Alterada a velocidade do projétil de 20 para 10
    
    - Tentativa de corrigir o bug de direção do projétil
    
    NOTE: Ainda não está 100% corrigido, necessário entender o porque ele ocorre

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,6 +10,7 @@ public class Projectile : MonoBehaviour
     private GameObject[] enemies;
     private Enemy enemy;
 
+    private EnemyPatrolling enemyPatrolling;
     private SpriteRenderer sprite;
 
     public Sprite[] projectileSprites;
@@ -48,14 +49,25 @@ public class Projectile : MonoBehaviour
             }
         }
         
-        var shooter = FindObjectOfType<ShootingProjectiles>();
-
-        float direction = 1f;
-        if (shooter != null)
+        for (int i = 0; i < enemies.Length; i++)
         {
-            direction = shooter.direction;
+            enemyPatrolling = enemies[i].GetComponentInChildren<EnemyPatrolling>();
+
+            if (Mathf.Abs(enemyPatrolling.playerTransform.position.x) - Mathf.Abs(enemyPatrolling.enemyTransform.position.x) >= -enemy.data.rangeAttack - 2.5f)
+            {
+                var shooter = enemies[i].GetComponentInChildren<ShootingProjectiles>();
+
+                float direction = 1f;
+                if (shooter != null)
+                {
+                    direction = shooter.direction;
+                }
+
+                rb.velocity = new Vector2(direction * speed, 0f);
+                break;
+            }
         }
 
-        rb.velocity = new Vector2(direction * speed, 0f);
+        
     }
 }
